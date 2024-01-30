e17eaf19c2b95b96e7a7f34b3d6a720860337741
brunotsane <luxcerebrum@gmail.com>
2024-01-29 21:47:56 -0500

    add cors

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 import express, { json } from 'express';
 import 'dotenv/config';
+import cors from 'cors';
 import nodemailer from 'nodemailer';
 
 
 const app = express();
+app.use(cors());
 app.use(json());
 app.use(express.static('Public'));
 
